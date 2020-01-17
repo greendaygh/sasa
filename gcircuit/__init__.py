e52f94b430873a1df0e5ad883bb9dac2d56e2f9f
unknown <neocaridina@sasa.hs.kr>
2020-01-17 12:27:23 +0900

    up

--- gcircuit/__init__.py
+++ gcircuit/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request
 from Bio.Seq import Seq
 from Bio.Alphabet import generic_dna
+from Bio import motifs
 import os
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches 
@@ -104,7 +105,42 @@ def show_gene(fig2, typ, leng, location):
         color='g'
         ))
     return None
-        
+def locate_geneAp(leng, location):
+        ax2.add_patch(
+        patches.Rectangle(
+        (location/100, 0.2),
+        int(leng)/1000,
+        0.1,
+        color='c'
+        #색에서 k가 검정, b 파랑, g 초록, c 청록(시앙!), m 마젠타, r 빨강, w 흰색, y 노란(?)색, 
+        ) ) 
+def locate_geneBr(leng, location):
+        ax2.add_patch(
+        patches.Rectangle(
+        (location/100, 0.2),
+        int(leng)/1000,
+        0.1,
+        color='b'
+        #색에서 k가 검정, b 파랑, g 초록, c 청록(시앙!), m 마젠타, r 빨강, w 흰색, y 노란(?)색, 
+        ) )
+def locate_geneBt(leng, location):
+        ax2.add_patch(
+        patches.Rectangle(
+        (location/100, 0.2),
+        int(leng)/1000,
+        0.1,
+        color='m'
+        #색에서 k가 검정, b 파랑, g 초록, c 청록(시앙!), m 마젠타, r 빨강, w 흰색, y 노란(?)색, 
+        ) )
+def locate_geneCr(leng, location):
+        ax2.add_patch(
+        patches.Rectangle(
+        (location/100, 0.2),
+        int(leng)/1000,
+        0.1,
+        color='y'
+        #색에서 k가 검정, b 파랑, g 초록, c 청록(시앙!), m 마젠타, r 빨강, w 흰색, y 노란(?)색, 
+        ) )        
 
 @app.route('/count')
 def count_from_html():
@@ -129,11 +165,25 @@ def save():
         bcds=Part(7, "ATC", "Bc", "CDS")
         drbs=Part(8, "CTC", "Dc", "CDS")
         ater=Part(9, "ATT", "At", "Terminator")
-        acircuit=Circuit(1, "circuit A", "biosensor")
+        bter=Part(9, "CTA", "At", "Terminator")
+        acircuit=Circuit(1, "circuit A", "biosensor")    
+
+        mAp = motifs.create([apro.seq])
+        mBr = motifs.create([brbs.seq])
+        mBt = motifs.create([bter.seq])
+        mCr = motifs.create([crbs.seq])
         acircuit.set_parts([apro,arbs,acds,apro,arbs,acds,acds,brbs,crbs,bcds,drbs,brbs,crbs, bcds, drbs, ater])
         acircuit.set_sequence()
-        acircuit.make_seq_figure()
-        full_filename = os.path.join(app.config['UPLOAD_FOLDER'], 'genegraphics.png')
+        for pos, seq in mAp.instances.search(acircuit.seq):         
+            locate_geneAp(len(seq),(pos))
+        for pos, seq in mBr.instances.search(acircuit.seq):
+            locate_geneBr(len(seq),(pos))
+        for pos, seq in mBt.instances.search(acircuit.seq):
+            locate_geneBt(len(seq),(pos))
+        for pos, seq in mCr.instances.search(acircuit.seq):
+            locate_geneCr(len(seq),(pos))
+        fig2.savefig("gcircuit/static/images/genegraphics2.png",format="png")
+        full_filename = os.path.join(app.config['UPLOAD_FOLDER'], 'genegraphics2.png')
         return render_template("graphic_test.html", image_file_name=full_filename) 
     else:
         return render_template("graphic_test.html", image_file_name="") 
